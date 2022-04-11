39daa54a5163d6f8b7837f7b84398be3cb935fc2
LuthyX <peteradegoke81@gmail.com>
2022-04-11 09:21:34 +0100

    added a new function for notfound.html

--- weather/views.py
+++ weather/views.py
@@ -1,22 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import  redirect, render
 import json
 import urllib.request
 
 
 # Create your views here.
+
 def index(request):
     if request.method  == 'POST':
-        city = request.POST['city']
-        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=e3eac307a35b3022f04a1ed7c2204e6c').read()
-        json_data = json.loads(res)
-        
-        data = {
-                'country_code' : str(json_data['sys']['country']),
-                'coordinate' : str(json_data['coord']['lon']) + '' + str(json_data['coord']['lat']),
-                'temp' : str(json_data['main']['temp']) + 'k',
-                'pressure' : str(json_data['main']['pressure']),
-                'humidity' : str(json_data['main']['humidity'])
-                    }
+        try:
+            city = request.POST['city']
+            res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=e3eac307a35b3022f04a1ed7c2204e6c').read()
+            json_data = json.loads(res)
+            
+            data = {
+                    'country_code' : str(json_data['sys']['country']),
+                    'coordinate' : str(json_data['coord']['lon']) + '' + str(json_data['coord']['lat']),
+                    'temp' : str(json_data['main']['temp']) + 'k',
+                    'pressure' : str(json_data['main']['pressure']),
+                    'humidity' : str(json_data['main']['humidity'])
+                        }
+        except:
+            return render(request, 'notfound.html')
         
         
         
@@ -25,3 +29,6 @@ def index(request):
         city = ''
         
     return render(request, 'index.html', {'data' : data, 'city':city })
+
+def notfound(request):
+    return render(request, 'notfound.html')
